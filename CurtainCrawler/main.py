42a3158f104e57b7ab909e7ef2ae318da9606536
jayvee-Air <jayveehe@gmail.com>
2016-01-22 00:13:17 +0800

    test main

--- CurtainCrawler/main.py
+++ CurtainCrawler/main.py
@@ -1,16 +1,17 @@
 # coding=utf-8
 
 from Crawler import Crawler
-from dao_utils import db_controller
+# from dao_utils import db_controller
 
 __author__ = 'wangzi6147'
 
 
 def main():
-    crawler = Crawler('http://www.douyutv.com/qiuri')
-    for userid, nickname, content, post_time, room_id in crawler.start():
-        print '[%s]%s: %s' % (post_time, nickname, content)
-        db_controller.save_danmu(userid, nickname, content, post_time, room_id)
+    crawler = Crawler('http://www.douyutv.com/67373')
+    crawler.start()
+    # for userid, nickname, content, post_time, room_id in crawler.start():
+    #     print '[%s]%s: %s' % (post_time, nickname, content)
+    #     db_controller.save_danmu(userid, nickname, content, post_time, room_id)
 
 
 if __name__ == '__main__':
