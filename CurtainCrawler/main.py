d4f953554c8013dc3203703279a6143b64f7be82
Jayvee-Air <jayveehe@gmail.com>
2016-06-23 11:50:24 +0800

    增加线程的OverWatcher类

--- CurtainCrawler/main.py
+++ CurtainCrawler/main.py
@@ -4,17 +4,34 @@ import time
 
 from Crawler import Crawler
 # from dao_utils import db_controller
+from home_page.crawler_monitor import OverWatcher
 
 __author__ = 'wangzi6147'
 
 
 def main():
-    crawler = Crawler('http://www.douyu.com/97376')
-    crawler.start()
-    # crawler.join()
-    time.sleep(10)
-    crawler.shutdown()
-    crawler.join()
+    crawler1 = Crawler('http://www.douyu.com/97376', '1')
+    crawler2 = Crawler('http://www.douyu.com/4809', '2')
+    crawler3 = Crawler('http://www.douyu.com/chuan967', '3')
+    ow = OverWatcher()
+    ow.add_task(crawler1)
+    print ow.show_all_task()
+    time.sleep(5)
+    ow.add_task(crawler2)
+    print ow.show_all_task()
+    time.sleep(5)
+    ow.add_task(crawler3)
+    print ow.show_all_task()
+    print 'stard done'
+    time.sleep(5)
+    ow.remove_task('1')
+    print ow.show_all_task()
+    time.sleep(5)
+    ow.remove_task('2')
+    print ow.show_all_task()
+    time.sleep(5)
+    # ow.remove_task('3')
+    print ow.show_all_task()
     print 'stoped'
     # for userid, nickname, content, post_time, room_id in crawler.start():
     #     print '[%s]%s: %s' % (post_time, nickname, content)
--- home_page/crawler_monitor.py
+++ home_page/crawler_monitor.py
@@ -1,6 +1,27 @@
+from common_utils.logging_utils import common_logger
+
 __author__ = 'jayvee'
 
 
-# class OverWatcher(object):
-    # def __init__(self):
-    #     super.__init__(OverWatcher)
+class OverWatcher(object):
+    def __init__(self):
+        self.running_tasks = {}
+
+    def add_task(self, crawler_thread):
+        common_logger.info('add task: %s' % crawler_thread.thread_id)
+        self.running_tasks[crawler_thread.thread_id] = crawler_thread
+        crawler_thread.start()
+
+    def remove_task(self, crawler_id):
+        crawler_thread = self.running_tasks.get(crawler_id, None)
+        if crawler_thread:
+            crawler_thread.shutdown()
+            del self.running_tasks[crawler_id]
+        else:
+            common_logger.error('no crawler named %s' % crawler_id)
+
+    def show_all_task(self):
+        res = []
+        for v in self.running_tasks.values():
+            res.append(v.url)
+        return res
